e9159adfdeacc6733fdb0ec6db49ca6a92c92bed
BalashovDV <korochun@yandex.ru>
2023-10-12 17:04:57 +0300

    Балашов Домашнее задание 5. 1. Создан файл linked_list.py с классом для односвязного списка 2. Созданы тесты для проверки функционала списка

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,29 +1,54 @@
 class Node:
-    """Класс для узла односвязного списка"""
-    pass
+	"""Класс для узла односвязного списка"""
+	
+	def __init__(self, data, next_node):
+		"""
+        Конструктор класса Node
+        :param data: данные, которые будут храниться в узле
+        """
+		
+		self.data = data
+		self.next_node = next_node
 
 
 class LinkedList:
-    """Класс для односвязного списка"""
-
-    def insert_beginning(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
-
-    def insert_at_end(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
-
-    def __str__(self) -> str:
-        """Вывод данных односвязного списка в строковом представлении"""
-        node = self.head
-        if node is None:
-            return str(None)
-
-        ll_string = ''
-        while node:
-            ll_string += f' {str(node.data)} ->'
-            node = node.next_node
-
-        ll_string += ' None'
-        return ll_string
+	"""Класс для односвязного списка"""
+	
+	def __init__(self):
+		'''
+        Инициируем список
+        '''
+		self.head = None
+	
+	def insert_beginning(self, data: dict) -> None:
+		"""Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
+		
+		node = Node(data, self.head)
+		self.head = node
+	
+	def insert_at_end(self, data: dict) -> None:
+		"""Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
+		
+		if self.head is None:
+			self.head = Node(data, None)
+			return
+		
+		node = self.head
+		while node.next_node:
+			node = node.next_node
+		node.next_node = Node(data, None)
+	
+	def __str__(self) -> str:
+		"""Вывод данных односвязного списка в строковом представлении"""
+		node = self.head
+		if node is None:
+			print(str(None))
+			return str(None)
+		
+		ll_string = ''
+		while node:
+			ll_string += f' {str(node.data)} ->'
+			node = node.next_node
+		
+		ll_string += ' None'
+		return ll_string.strip()
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,34 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+
+import unittest
+
+"""
+Импорт модуля, содержащего функции для тестирования
+"""
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+	'''
+	Класс для тестирования класса по работе со связанным списком
+	'''
+	
+	def setUp(self) -> None:
+		self.ll = LinkedList()
+	
+	def test_str(self):
+		self.assertEqual(str(self.ll), 'None')
+		self.ll.insert_beginning({'id': 1})
+		self.assertEqual(str(self.ll), "{'id': 1} -> None")
+	
+	def test_insert_beginning(self):
+		self.ll.insert_beginning({'id': 0})
+		self.assertEqual(str(self.ll), "{'id': 0} -> None")
+	
+	def test_insert_at_end(self):
+		self.ll.insert_at_end({'id': 2})
+		self.assertEqual(str(self.ll), "{'id': 2} -> None")
+		self.ll.insert_at_end({'id': 3})
+		self.assertEqual(str(self.ll), "{'id': 2} -> {'id': 3} -> None")
+		self.ll.insert_at_end({'id': 4})
+		self.assertEqual(str(self.ll), "{'id': 2} -> {'id': 3} -> {'id': 4} -> None")
